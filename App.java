b9079f24ef838385e1634d8b94752d9fbd4b24f0
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-11-26 01:36:54 -0700

    Change main

--- App.java
+++ App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        AbstractFactory computerFactory = FactoryProducer.getFactory(false);
-        Computer computer = computerFactory.getComputer("SmartTv");
+        AbstractFactory computerFactory = FactoryProducer.getFactory(true);
+        Computer computer = computerFactory.getComputer("Laptop");
         computer.compute();
+        
+        Computer computer2 = computerFactory.getComputer("Phone");
+
+        computer2.compute();
     }
 }
